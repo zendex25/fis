118a6442cdfc06dc9f1f8ea1e8be14943d53602d
zendex <zendex@ubuntu.(none)>
2014-02-08 17:53:05 +0800

    Fresh.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fis::Application.config.secret_key_base = 'ccd2dc5c5a1b3be3cdbba44a8c5c4b2b074da7426a7e4f155fdc53d712dd04d7e7372baa9658bb9057eaaf5358b12488bc2082792dd3c64a5950610623431060'
