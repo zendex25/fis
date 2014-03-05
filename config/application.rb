b72eea0fa9a894c99d870b23972b6d64beabd471
zendex25 <zendex25@yahoo.com>
2014-03-06 00:06:17 +0800

    asdf

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Fis
 
     config.time_zone = "Singapore"
     # Heroku requires this to be false
-    config.assets.initialize_on_precompile=false
+    config.assets.initialize_on_precompile=true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
