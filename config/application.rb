ab1cded75fabda5a91928b9a06eaea520f67a642
zendex25 <zendex25@yahoo.com>
2014-03-05 23:55:45 +0800

    modified config.assets in app and env

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Fis
     config.time_zone = "Singapore"
     # Heroku requires this to be false
     config.assets.initialize_on_precompile=false
+    config.assets.precompile += %w(active_admin.css active_admin.js)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
