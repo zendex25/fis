b27999c1b7da10d64b49d0ef8ca4a7b2756b3c9b
zendex25 <zendex25@yahoo.com>
2014-03-12 00:23:23 +0800

    temp removed absence.rb content

--- app/admin/absence.rb
+++ app/admin/absence.rb
@@ -1,32 +1,7 @@
 ActiveAdmin.register Absence do
 
   
-index do
-  column :faculty do |faculty|
-    faculty.faculty.fullname
-  end
-  column :date_of_absence
-  column :on_leave
-  default_actions
-end
 
-filter :faculty_id, :as => :select, :collection => Faculty.all.map {|u| [u.fullname, u.id]}, :include_blank => false
-filter :on_leave
-
-form do |f|
-  f.inputs "Absence" do
-    f.input :faculty_id, :as => :select, :collection => Faculty.all.map {|u| [u.last_name, u.id]}, :include_blank => false
-    f.input :date_of_absence, :as => :date_select
-    f.input :on_leave, :as => :select,
-      :collection => 
-        [['Annual Leave','Annual Leave'],
-        ['Sick Leave','Sick Leave'],
-        ['No Pay Leave','No Pay Leave'],
-        ['Paternal or Maternal Leave','Paternal or Maternal Leave'],
-        ['Berievement Leave','Berievement Leave']], :include_blank => true, :label => 'On Leave'
-  end
-f.actions
-end
 
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
